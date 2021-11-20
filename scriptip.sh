5ce68e4b543ab8d115a7040e4c7c1b82b3ef0a72
Joachim T <40643102+osirisbash@users.noreply.github.com>
2021-11-20 23:41:11 +0100

    Add files via upload

--- scriptip.sh
+++ scriptip.sh
@@ -1,45 +1,42 @@
 ﻿#!/bin/sh
 
-iptables -P INPUT DROP 
-iptables -P OUTPUT DROP 
-iptables -P FORWARD DROP
+IPTABLES="/usr/sbin/iptables"
+
+# Variables loopback local
+IFACE_LO="lo"
+IP_FW_LO="127.0.0.1"
+
+NET_ALL_INTERNE="192.168.0.0/16"
+
+# Variables pour la connexion Internet
+IFACE_INTERNET="ens4"
+IP_FW_INTERNET="192.168.1.1"
 
 
+IFACE_PDT="ens6"
+IFACE_VLAN_PDT_ADMIN="ens6.30"
+IP_FW_VLAN_PDT_ADMIN="192.168.30.1"
+NET_PDT_ADMIN="192.168.30.0/24"
 
-# LO
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-iptables -A FORWARD -i lo -j ACCEPT
-iptables -A FORWARD -o lo -j ACCEPT
+# Definition de la politique par defaut
+$IPTABLES -P INPUT DROP
+$IPTABLES -P OUTPUT DROP
+$IPTABLES -P FORWARD DROP
 
-
-iptables -A FORWARD -t filter -m state --state ESTABLISHED,RELATED 
- 
- 
-# PING VERS PASSERELLE
-
-iptables -A INPUT -t filter -s 192.168.30.0/24 -d 192.168.30.1 -i ens6.30 -p icmp --icmp-type echo-request -j ACCEPT 
-iptables -A OUTPUT -t filter -p icmp --icmp-type echo-reply -j ACCEPT 
-iptables -A INPUT -t filter -s 195.168.1.0/24 -d 195.168.1.1 -i ens4 -p icmp --icmp-type echo-request -j ACCEPT
+# Autorisation du trafic pour l'interface locale de Loopback
+$IPTABLES -A INPUT -p ALL -i $IFACE_LO -s $IP_FW_LO -j ACCEPT
+$IPTABLES -A INPUT -p ALL -i $IFACE_LO -s $NET_ALL_INTERNE -j ACCEPT
+$IPTABLES -A INPUT -p ALL -i $IFACE_LO -s $IP_FW_INTERNET -j ACCEPT
 
 
-#POSTROUTING  
-iptables -A POSTROUTING -t nat -o ens4 -j MASQUERADE 
-#iptables ­t nat ­A POSTROUTING ­-s <adrIP ou réseau à masquer> ­-o <interface ext.> -­j MASQUERADE
-iptables -A POSTROUTING -t nat -s 192.168.30.0/24 -j SNAT --to-source 192.168.1.1 
+# NAT pour les echanges entre le proxy Web et Internet
+$IPTABLES -t nat -A POSTROUTING -o $IFACE_DMZOUT -j MASQUERADE
 
-# ICMP
-iptables -A FORWARD -t filter -s 192.168.30.0/24 -i ens6.30 -o ens4 -p icmp --icmp-type echo-request -j ACCEPT 
-iptables -A FORWARD -t filter -i ens4 -o ens6.30 -p icmp --icmp-type echo-reply -j ACCEPT 
+$IPTABLES -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-
-# HTTP & HTTPS
-iptables -A FORWARD -t filter -s 192.168.30.0/24 -i ens6.30 -o ens4 -p tcp  --dport 80 -j ACCEPT 
-iptables -A FORWARD -t filter -s 192.168.30.0/24 -i ens6.30 -o ens4 -p tcp  --dport 443 -j ACCEPT 
-iptables -A FORWARD -t filter -i ens4 -d 192.168.30.0/24 -p tcp --sport 443 -j ACCEPT 
-iptables -A FORWARD -t filter -i ens4 -d 192.168.30.0/24 -p tcp --sport 80 -j ACCEPT
-    
-$IPTABLES -A FORWARD -p TCP -i ens6.30 -o ens4 -s 192.168.30.5 -m multiport --dport 80,443 -j ACCEPT -j LOG --log-prefix "Surf Web:   
+# Acces Web depuis les postes de travail vers le proxy
+$IPTABLES -A FORWARD -p TCP -i $IFACE_PDT -o $IFACE_DMZOUT -d $IP_DMZOUT_PROXY --dport 8080 -j ACCEPT -j LOG --log-prefix "Surf Web: "
 
 
-
+#puis Proxy vers Internet
+$IPTABLES -A FORWARD -p TCP -i $IFACE_VLAN_PDT_ADMIN -o $IFACE_INTERNET -s $192.168.30.5 -m multiport --dport 80,443 -j ACCEPT -j LOG --log-prefix "Surf Web: "
